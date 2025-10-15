95d7bb50a8b94722d2e342452085132168639227
Amon <arne.werner87@gmail.com>
2025-10-15 07:22:39 +0000

    de update

--- codex-site/docusaurus.config.ts
+++ codex-site/docusaurus.config.ts
@@ -143,7 +143,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Us. Built with Love.`,
     },
     prism: {
       theme: prismThemes.github,
